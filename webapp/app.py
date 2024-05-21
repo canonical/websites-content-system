09ce72f10194ec3a80e087b24a5c3c4bd437676f
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2024-05-21 22:49:01 +0500

    add CORS header in development mode

--- webapp/app.py
+++ webapp/app.py
@@ -2,8 +2,9 @@ from flask import jsonify
 
 from webapp import create_app
 from webapp.cache import Cache
-# from webapp.parser_task import ParserTask
+from webapp.parser_task import ParserTask
 from webapp.site_repository import SiteRepository, SiteRepositoryError
+from os import environ
 
 app = create_app()
 
@@ -15,7 +16,7 @@ except ConnectionError:
     cache = None
 
 # Start parser task
-# parser_task = ParserTask(app, cache=cache)
+parser_task = ParserTask(app, cache=cache)
 
 
 @app.route("/get-tree/<string:uri>", methods=["GET"])
@@ -28,7 +29,13 @@ def region(uri, branch="main"):
 
     tree = site_repository.get_tree()
 
-    return jsonify({"name": uri, "templates": tree})
+    response = jsonify({"name": uri, "templates": tree})
+
+    DEVELOPMENT_MODE = environ.get("DEVEL", True)
+    if DEVELOPMENT_MODE:
+        response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return response
 
 
 # TODO: WHEN cache NOT AVAILABLE, should not cause connection errors
