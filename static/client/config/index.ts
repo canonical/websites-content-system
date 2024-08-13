77290d3360dac3de2f015ea5eb5e5b69b94d13d9
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2024-08-13 10:16:49 +0500

    fix bug with opening local routes

--- static/client/config/index.ts
+++ static/client/config/index.ts
@@ -3,7 +3,7 @@ let staleTime = process.env.NODE_ENV === "production" ? 300000 : 30000;
 const config = {
   projects: ["canonical.com", "ubuntu.com"],
   api: {
-    path: window.location.href,
+    path: "/",
     FETCH_OPTIONS: {
       retry: false,
       refetchOnWindowFocus: false,
