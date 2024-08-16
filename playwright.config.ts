f814a4e9801b656e726b9d38f270a014fc8cf0fb
Olwe Samuel <sam.olwe@gmail.com>
2024-08-16 10:18:58 +0300

    Added defaults to playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,6 +33,14 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
 
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+
   /* Configure projects for major browsers */
   projects: [
     {
