0193660b9be98e9ad03ae8310f92839fcf57ac38
immortalcodes <21112002mj@gmail.com>
2024-10-16 17:28:17 +0530

    made login necessary

--- webapp/app.py
+++ webapp/app.py
@@ -182,6 +182,7 @@ def get_jira_tasks(webpage_id: int):
 
 @app.route("/api/remove_webpage", methods=["POST"])
 @validate()
+@login_required
 def remove_webpage(body: RemoveWebpageModel):
     """
     Remove a webpage based on its status.
