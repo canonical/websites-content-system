113992c8e540fd94b0c12ae14f80a5074dc6848d
Olwe Samuel <sam.olwe@gmail.com>
2024-11-27 15:23:51 +0300

    Disable cache for specific url

--- webapp/app.py
+++ webapp/app.py
@@ -56,8 +56,10 @@ def get_tree(uri: str, branch: str = "main", no_cache: bool = False):
         }
     )
 
-    DEVELOPMENT_MODE = environ.get("DEVEL", True)
-    if DEVELOPMENT_MODE:
+    # Disable caching for this response
+    response.cache_control.no_store = True
+    # Allow CORS in development mode
+    if app.config["DEVELOPMENT_MODE"]:
         response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
