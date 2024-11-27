40942778f613d573dff25bde06f4de5355a77c96
Olwe Samuel <samuel.olwe@canonical.com>
2024-11-27 15:26:06 +0300

    Update __init__.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,23 +10,12 @@ from webapp.sso import init_sso
 from webapp.tasks import init_tasks
 
 
-def set_cache_control_headers(response):
-    if flask.request.path.startswith("/api/get-tree"):
-        # Our tree endpoints need to be uncached
-        # to prevent caching empty trees. We instead use
-        # a locally implemented cache for tree responses
-        response.cache_control.no_store = True
-
-
 def create_app():
     app = Flask(
         __name__, template_folder="../templates", static_folder="../static"
     )
     app.config.from_pyfile("settings.py")
     app.context_processor(base_context)
-
-    # Set cache control headers
-    app.after_request(set_cache_control_headers)
 
     # Initialize database
     init_db(app)
